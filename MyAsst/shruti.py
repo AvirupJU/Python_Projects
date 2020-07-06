651154e10efa76bd0d8b8416502eb02a063804f4
Avirup Dey <54141630+AvirupJU@users.noreply.github.com>
2020-07-06 10:45:45 +0530

    Add files via upload

---
+++ MyAsst/shruti.py
@@ -0,0 +1,116 @@
+import os
+import smtplib
+import webbrowser
+import wikipedia
+import pyttsx3 
+import speech_recognition as sr
+import datetime
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)#change to [0] for male voice
+engine.setProperty('rate', 130)
+
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am SHRUTI, your custom virtual assistant. How may I help you?")
+
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Processing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"You said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    
+        query = takeCommand().lower()
+
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=3)
+            speak("Wikipedia Says")
+            print(results)
+            speak(results)
+
+        elif 'I love you' in query:
+            speak("I love you too, sir. I wish to stay with you for all my life.")
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open github' in query:
+            webbrowser.open("github.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+     
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+        elif 'the date' in query:
+            strDate = datetime.datetime.now().strftime("%m-%d-%Y")    
+            speak(f"Sir, the date is {strDate}")
+
+
+        #elif 'open <program name>' in query:
+            #codePath = "Add path of any program"
+            #os.startfile(codePath)
+
+        elif 'email to Friend' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "dummy@gmail.com"    
+                sendEmail(to, content)
+                speak("Email sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir. I am not able to send this email")    
